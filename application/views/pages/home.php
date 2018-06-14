a72525135edf50fdb53476788c102836e4245591
donzaburo <donzaburo@gmail.com>
2018-06-14 16:53:27 +0900

    aafeaf

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-ここはホームだったよ
-<p><a href="<?php echo site_url('about/'); ?>">aboutを見る</a></p>
-=======
 aaaここはホームだよ
 <p><a href="<?php echo site_url('about/'); ?>">aboutを見る</a></p>
->>>>>>> origin/master
